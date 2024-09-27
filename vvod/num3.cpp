7de9b0b8070107d67af016994caa9bea39542444
user.name <vianmart@yandex.ru>
2024-09-27 16:32:33 +0300

    changed num3 to ordered instead of lock

--- vvod/num3.cpp
+++ vvod/num3.cpp
@@ -5,16 +5,14 @@
 int main(int argc, char **argv) {
     int all = omp_get_max_threads();
     int counter = 0;
-    omp_lock_t lock;
-    omp_init_lock(&lock);
 
-#pragma omp parallel
-    {
-        while (!omp_test_lock(&lock)) ;
+#pragma omp parallel for ordered
+    for (int i=0; i<all; i++) {
+        #pragma omp ordered
+        {
         printf("thread %d, cntr is %d\n", omp_get_thread_num(), counter);
         counter++;
-        omp_unset_lock(&lock);
+        }
     }
     printf("final counter is %d\n", counter);
-    omp_destroy_lock(&lock);
 }
